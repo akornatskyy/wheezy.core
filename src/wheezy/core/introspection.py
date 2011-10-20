e52df4666a98cb865b9f96099da52ada6f958d82
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-10-20 20:44:38 +0200

    Added import_name.

---
+++ src/wheezy/core/introspection.py
@@ -0,0 +1,16 @@
+
+""" ``introspection`` module.
+"""
+
+from wheezy.core.comp import __import__
+
+
+def import_name(fullname):
+    """
+        >>> from datetime import timedelta
+        >>> import_name('datetime.timedelta') is timedelta
+        True
+    """
+    namespace, name = fullname.rsplit('.', 1)
+    obj = __import__(namespace, None, None, [name])
+    return getattr(obj, name)
