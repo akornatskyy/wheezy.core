d3f33be241469d2a26285007baee3e73b1cc1458
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2013-10-13 13:24:26 +0300

    Added demo for httpclient.

---
+++ demos/httpclient/api.py
@@ -0,0 +1,10 @@
+"""
+"""
+
+from wheezy.core.httpclient import HTTPClient
+
+
+client = HTTPClient('http://buildbot.buildbot.net/json/')
+assert 200 == client.get('project')
+project = client.json
+assert 'Buildbot' == project.title
